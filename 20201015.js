aafede5e6d3072f7461d373715e843e0dd46e84f
Hany Afifi <hanyafifi@gmail.com>
2020-10-26 20:23:06 +0100

    ADDED: 20201015.js lecture exercise

---
+++ 20201015.js
@@ -0,0 +1,15 @@
+/*Recap 2 - arrays 
+* Let's create an array of 5 elements
+* iterate over it with a for loop
+* and print out each element separately 
+*/
+
+
+
+function recap1() {
+    const array = [1, 2, 3, 4, 5, 6, 7];
+    
+    for (let i = 0; i < array.length; i++ ) {
+        console.log(array[i]);
+    }
+}
---
+++ script1.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+/* WHILE loop */
+
+/* var num = 0;
+while (num < 100){
+    num += 1;   // if this is deleted the while will run forever causing problems!
+    console.log(num);
+}
+ */
+
+
+/* and to change that to FOR loop */
+/* for (let num = 0; num <= 100; num++){
+    console.log(num);
+}
+ */
+
+
+
+ /*  DATA TYPES */
+
+ var yourAge = 18;   // number
+ let yourName = 'Hany';   // string
+ let name = {first: 'Hany', last: 'Afifi'}; // object
+
+
+
